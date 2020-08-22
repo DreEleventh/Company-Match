2ec2cd4d61c5d25b98d81da02e47023c1708446b
Andre amacKenzie <andremckenzie58@gmail.com>
2020-08-22 16:10:17 -0500

    "Initial commit. Created the company, employee and emplyer dictionaries and populated them"

---
+++ company_match.py
@@ -0,0 +1,18 @@
+# This GUI program is built using tkinter and python users are 
+# asked to select a person then a company and varify is they match 
+# they only match if the person works for that company 
+
+import tkinter as tk 
+
+# Creating a set of list and dictionaries to hold the employees and their employors
+company = {"JSN Bank":"Kim Anderson", "Kingston Industrial":"Andrew Peterson", 
+            "Amazon Jamaica":"Stephen Jones", "Ministry of Health":"Roy Simpson", 
+            "Whole Foods":"Keisha Tolcut", "Daily Radio":"John Anderson", 
+            "ABN Enterprises":"Yvonne Bryan", "PLB Planet":"Rick Grant"}
+
+employee =["John Anderson","Kim Anderson", "Rick Grant", "Andrew Peterson", 
+            "Yvonne Bryan", "Stephen Jones", "Roy Simpson", "Keisha Tolcut"]
+
+employer =["PLB Planet", "JSN Bank", "Kingston Industrial", "ABN Enterprises", 
+            "Amazon Jamaica", "Daily Radio", "Ministry of Health", "Whole Foods"]
+
